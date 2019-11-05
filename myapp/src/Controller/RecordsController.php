5a5f226d4289467a0a9137af22556c7240be86f0
Darius Valancauskas <darius.veracity@gmail.com>
2019-11-05 15:44:40 +0200

    Post accepting data from parameter

--- myapp/src/Controller/RecordsController.php
+++ myapp/src/Controller/RecordsController.php
@@ -2,20 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Record;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RecordsController
+class RecordsController extends AbstractController
 {
     /**
      * @Route("/save", name="save")
+     * @param Request $request
+     * @return Response
      */
-    public function save()
+    public function save(Request $request)
     {
-        $number = 1;
+        $entityManager = $this->getDoctrine()->getManager();
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        $content = json_decode($request->getContent(), true);
+
+        $record = new Record();
+        $record->setName($content['name']);
+        $record->setPrice(rand(1, 200));
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($record);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$record->getId());
     }
 }
--- myapp/src/Entity/Record.php
+++ myapp/src/Entity/Record.php
@@ -32,4 +32,36 @@ class Record
     }
 
     // ... getter and setter methods
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price): void
+    {
+        $this->price = $price;
+    }
 }
