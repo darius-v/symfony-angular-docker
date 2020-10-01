8aa7208fbf089819288b088c0668a2eedbc52af6
Darius Valancauskas <darius.veracity@gmail.com>
2020-10-01 18:27:17 +0300

    New record form

--- myapp/public/RecordsController.js
+++ myapp/public/RecordsController.js
@@ -9,6 +9,13 @@ app.config(function($interpolateProvider) {
 
 app.controller('RecordsController', ['$scope', '$http', function($scope, $http) {
 
+    $scope.submit = function () {
+        $http.post(app.baseUrl + 'add')
+            .then(function(response) {
+                $scope.records.push(response.data); // todo
+            });
+    }
+
     $http.get(app.baseUrl + 'list')
         .then(function(response) {
             $scope.records = response.data;
